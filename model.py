761dbd409b485fda12d3638a6eb71f22a4318228
enricguso <enricguso@gmail.com>
2020-04-10 20:35:35 +0200

    Delete Softmask and add comments

--- model.py
+++ model.py
@@ -4,47 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
 
-class SoftMask(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        """Separates a mixture with a ratio mask, using the provided sources
-        spectrograms estimates. Additionally allows compressing the mask with
-        a logit function for soft binarization.
-        The filter does *not* take multichannel correlations into account.
-        The masking strategy can be traced back to the work of N. Wiener in the
-        case of *power* spectrograms [1]_. In the case of *fractional* spectrograms
-        like magnitude, this filter is often referred to a "ratio mask", and
-        has been shown to be the optimal separation procedure under alpha-stable
-        assumptions [2]_.
-        References
-        ----------
-        .. [1] N. Wiener,"Extrapolation, Inerpolation, and Smoothing of Stationary
-            Time Series." 1949.
-        .. [2] A. Liutkus and R. Badeau. "Generalized Wiener filtering with
-            fractional power spectrograms." 2015 IEEE International Conference on
-            Acoustics, Speech and Signal Processing (ICASSP). IEEE, 2015.
-        Parameters
-        ----------
-        """
-        # to avoid dividing by zero
-        # create the soft mask as the ratio of the spectrograms with their sum
-    def forward(self, v, x):
-        eps = torch.finfo(x.dtype).eps
-        nb_sources, nb_frames, nb_samples, nb_channels, nb_bins = v.shape
-        x = x.reshape(nb_frames*nb_samples, nb_channels, nb_bins, 2)
-        x = x.permute(0,2,1,3)
-        v = v.reshape(nb_sources, nb_frames*nb_samples, nb_channels, nb_bins)
-        v = v.permute(1,3,2,0)
-        out = x[..., None] * (v / (eps + torch.sum(v, dim=-1, keepdim=True).to(x.dtype)))[..., None, :]
-        out = out.reshape(nb_frames, nb_samples, nb_bins, nb_channels, 2, nb_sources)
-        out = out.permute(5, 0, 1, 3, 2, 4)
-        #outputs: souce, frames, batch, channels, bins, 2(complex)
-        return out
-
-
-
-
 class NoOp(nn.Module):
     def __init__(self):
         super().__init__()
@@ -65,10 +24,10 @@ class ISTFT(nn.Module):
         self.center = center
     def forward(self, Estimates, mixaudiolen):
         """
-        Input:
-        Estimates (nb_frames, nb_samples, nb_channels, nb_bins, 2)
+        Parameters:
+        Estimates, float32 of the shape [nb_frames, nb_samples, nb_channels, nb_bins, 2]
         mixaudiolen: int, with the length of the original mixture for adjusting right padding x.shape[-1]
-        Output:(nb_samples, nb_channels, nb_timesteps)
+        Output: time-domain floats with shape [nb_samples, nb_channels, nb_timesteps]
         """
         nb_frames, nb_samples, nb_channels, nb_bins, _ = Estimates.size()
         device = Estimates.device
@@ -125,176 +84,6 @@ class STFT(nn.Module):
         return stft_f
 
 
-class Spectrogram(nn.Module):
-    def __init__(
-        self,
-        power=1,
-        mono=True
-    ):
-        super(Spectrogram, self).__init__()
-        self.power = power
-        self.mono = mono
-
-    def forward(self, spec):
-        """
-        Input: complex STFT
-            (nb_samples, nb_channels, nb_bins, nb_frames, 2)
-        Output: Power/Mag Spectrogram
-            (nb_frames, nb_samples, nb_channels, nb_bins)
-        """
-        #stft_f = stft_f.transpose(2, 3)
-        if self.mono:
-            spec =  torch.mean(spec, 1, keepdim=True)
-        magnitude = torchaudio.functional.complex_norm(spec)  # get power of "complex" tensor
-
-        # permute output for LSTM convenience
-        return magnitude.permute(3, 0, 1, 2)
-
-
-class OpenUnmix(nn.Module):
-    def __init__(
-        self,
-        n_fft=4096,
-        n_hop=1024,
-        input_is_spectrogram=False,
-        hidden_size=512,
-        nb_channels=2,
-        sample_rate=44100,
-        nb_layers=3,
-        input_mean=None,
-        input_scale=None,
-        max_bin=None,
-        unidirectional=False,
-        power=1,
-    ):
-        """
-        Input: (nb_samples, nb_channels, nb_timesteps)
-            or (nb_frames, nb_samples, nb_channels, nb_bins)
-        Output: Power/Mag Spectrogram
-                (nb_frames, nb_samples, nb_channels, nb_bins)
-        """
-
-        super(OpenUnmix, self).__init__()
-
-        self.hidden_size = hidden_size
-
-        self.nb_output_bins = n_fft // 2 + 1
-        if max_bin:
-            self.nb_bins = max_bin
-        else:
-            self.nb_bins = self.nb_output_bins
-
-        self.register_buffer('sample_rate', torch.tensor(sample_rate))
-
-        self.fc1 = Linear(
-            self.nb_bins*nb_channels, hidden_size,
-            bias=False
-        )
-
-        self.bn1 = BatchNorm1d(hidden_size)
-
-        if unidirectional:
-            lstm_hidden_size = hidden_size
-        else:
-            lstm_hidden_size = hidden_size // 2
-
-        self.lstm = LSTM(
-            input_size=hidden_size,
-            hidden_size=lstm_hidden_size,
-            num_layers=nb_layers,
-            bidirectional=not unidirectional,
-            batch_first=False,
-            dropout=0.4,
-        )
-
-        self.fc2 = Linear(
-            in_features=hidden_size*2,
-            out_features=hidden_size,
-            bias=False
-        )
-
-        self.bn2 = BatchNorm1d(hidden_size)
-
-        self.fc3 = Linear(
-            in_features=hidden_size,
-            out_features=self.nb_output_bins*nb_channels,
-            bias=False
-        )
-
-        self.bn3 = BatchNorm1d(self.nb_output_bins*nb_channels)
-
-        if input_mean is not None:
-            input_mean = torch.from_numpy(
-                -input_mean[:self.nb_bins]
-            ).float()
-        else:
-            input_mean = torch.zeros(self.nb_bins)
-
-        if input_scale is not None:
-            input_scale = torch.from_numpy(
-                1.0/input_scale[:self.nb_bins]
-            ).float()
-        else:
-            input_scale = torch.ones(self.nb_bins)
-
-        self.input_mean = Parameter(input_mean)
-        self.input_scale = Parameter(input_scale)
-
-        self.output_scale = Parameter(
-            torch.ones(self.nb_output_bins).float()
-        )
-        self.output_mean = Parameter(
-            torch.ones(self.nb_output_bins).float()
-        )
-
-    def forward(self, x):
-        # check for waveform or spectrogram
-        # transform to spectrogram if (nb_samples, nb_channels, nb_timesteps)
-        # and reduce feature dimensions, therefore we reshape
-        nb_frames, nb_samples, nb_channels, nb_bins = x.data.shape
-
-        # crop
-        x = x[..., :self.nb_bins]
-
-        # shift and scale input to mean=0 std=1 (across all bins)
-        x += self.input_mean
-        x *= self.input_scale
-
-        # to (nb_frames*nb_samples, nb_channels*nb_bins)
-        # and encode to (nb_frames*nb_samples, hidden_size)
-        x = self.fc1(x.reshape(-1, nb_channels*self.nb_bins))
-        # normalize every instance in a batch
-        x = self.bn1(x)
-        x = x.reshape(nb_frames, nb_samples, self.hidden_size)
-        # squash range ot [-1, 1]
-        x = torch.tanh(x)
-
-        # apply 3-layers of stacked LSTM
-        lstm_out = self.lstm(x)
-
-        # lstm skip connection
-        x = torch.cat([x, lstm_out[0]], -1)
-
-        # first dense stage + batch norm
-        x = self.fc2(x.reshape(-1, x.shape[-1]))
-        x = self.bn2(x)
-
-        x = F.relu(x)
-
-        # second dense stage + layer norm
-        x = self.fc3(x)
-        x = self.bn3(x)
-
-        # reshape back to original dim
-        x = x.reshape(nb_frames, nb_samples, nb_channels, self.nb_output_bins)
-
-        # apply output scaling
-        x *= self.output_scale
-        x += self.output_mean
-
-        # since our output is non-negative, we can apply RELU
-        return F.relu(x)
-
 class OpenUnmixSingle(nn.Module):
     def __init__(
         self,
@@ -328,16 +117,8 @@ class OpenUnmixSingle(nn.Module):
         self.hidden_size = hidden_size
 
         self.stft = STFT(n_fft=n_fft, n_hop=n_hop)
-        self.spec = Spectrogram(power=power, mono=(nb_channels == 1))
         self.istft = ISTFT(n_fft=n_fft, n_hop=n_hop)
         self.register_buffer('sample_rate', torch.tensor(sample_rate))
-
-        if input_is_spectrogram:
-            self.transform = NoOp()
-        else:
-            self.transform = nn.Sequential(self.stft, self.spec)
-
-        self.softmask = SoftMask()
 
         self.fc1 = Linear(
             self.nb_bins*nb_channels, hidden_size,
@@ -422,16 +203,12 @@ class OpenUnmixSingle(nn.Module):
             torch.ones(self.nb_output_bins).float()
         )
 
-        #declare softmax for sum(masks)=1
-        #self.softmax = torch.nn.Softmax(0)
-
 
     def forward(self, x):
         # check for waveform or spectrogram
         # transform to spectrogram if (nb_samples, nb_channels, nb_timesteps)
         # and reduce feature dimensions, therefore we reshape
         x = self.transform(x)
-        #mix = x.detach().clone()
         nb_frames, nb_samples, nb_channels, nb_bins = x.data.shape
 
         # crop
@@ -496,45 +273,4 @@ class OpenUnmixSingle(nn.Module):
         x_3 = F.relu(x_3)
         x_4 = F.relu(x_4)
 
-        '''
-        masks = [x_1, x_2, x_3, x_4]
-
-        x_1 = masks[0] * mix
-        x_2 = masks[1] * mix
-        x_3 = masks[2] * mix
-        x_4 = masks[3] * mix
-        '''
-
         return [x_1, x_2, x_3, x_4]
-
-
-class OpenUnmixJoint(nn.Module):
-    def __init__(
-        self,
-        targets,
-        *args, **kwargs
-    ):
-        super(OpenUnmixJoint, self).__init__()
-
-        self.stft = STFT(n_fft=4096, n_hop=1024)
-        self.spec = Spectrogram(power=1, mono=False)
-
-        self.transform = nn.Sequential(self.stft, self.spec)
-
-        self.models = nn.ModuleList(
-            [OpenUnmix(*args, **kwargs) for target in targets]
-        )
-        self.softmax = torch.nn.Softmax(0)
-
-    def forward(self, x):
-        X = self.transform(x)
-
-        logit_mask_list = []
-        for umx_single_source in self.models:
-            logit_mask_list.append(umx_single_source(X))
-
-        masks = self.softmax(torch.stack(logit_mask_list))
-        out = []
-        for i, models in enumerate(self.models):
-            out.append(masks[i, ...] * X)
-        return out
